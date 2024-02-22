afec34f19c32ec1e500c3f269637c744f53e3b33
Hiroki Fujii <hfujii@hisystron.com>
2024-02-23 00:14:29 +0900

    print削除

--- addon/globalPlugins/USB-D/inputManagerPatch.py
+++ addon/globalPlugins/USB-D/inputManagerPatch.py
@@ -16,11 +16,11 @@ class InputManagerPatch():
         # turn over routing switch
         if isinstance(getattr(gesture, "routingIndex", None), int) and braille.handler and braille.handler.display and gesture.routingIndex >= 0 and gesture.routingIndex < braille.handler.display.numCells:
             gesture.routingIndex = braille.handler.display.numCells - gesture.routingIndex - 1
-        print("key = %s, %s, %s, %s" %(getattr(gesture, "id", ""), getattr(gesture, "space", ""), getattr(gesture, "dots", ""), getattr(gesture, "source", "")))
+        #print("key = %s, %s, %s, %s" %(getattr(gesture, "id", ""), getattr(gesture, "space", ""), getattr(gesture, "dots", ""), getattr(gesture, "source", "")))
         # input key convert
         if hasattr(gesture, "id") and hasattr(gesture, "routingIndex"):
             for k in KEY_TABLE.get(gesture.source, []):
                 gesture.id = re.sub(r'(^|\+)%s(\+|$)' %(k[0],), r'\1NVDA0000ACTLAB_%s\2' %(k[1],), gesture.id)
             gesture.id = gesture.id.replace("NVDA0000ACTLAB_", "")
-        print("key = %s, %s, %s" %(getattr(gesture, "id", ""), getattr(gesture, "space", ""), getattr(gesture, "dots", "")))
+        #print("key = %s, %s, %s" %(getattr(gesture, "id", ""), getattr(gesture, "space", ""), getattr(gesture, "dots", "")))
         InputManagerPatch.executeGestureOriginal(self, gesture)
